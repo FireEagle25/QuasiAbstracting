c62683d0dd4c801e2a8c88dd2aa6b79b45a09c01
WinPooh32 <winpooh32@yandex.ru>
2016-12-06 21:51:05 +1000

    Исправлены очепятки

--- Abstractor/Abstractor.py
+++ Abstractor/Abstractor.py
@@ -3,14 +3,16 @@ import math
 
 from Abstractor.StringSlicing import get_words, get_sentences
 from Abstractor.WordStorage import WordStorage
+from Abstractor.WordsDictionary import WordsDictionary
 
 
 class Abstractor:
 
     def __init__(self, text):
         self.sentences = get_sentences(text)
-        self.world_storage = WordStorage()
-        self.world_storage.add_from_text(text)
+        self.word_storage = WordStorage()
+        self.word_storage.add_from_text(text)
+        self.words_dictionary = WordsDictionary("rus_words_db.pickle")
 
     def truncate(self, prescinde_percent=0.3):
 
@@ -18,8 +20,9 @@ class Abstractor:
         sentences_count = sentences_count if sentences_count > 0 else 1
 
         weights = self.__get_sentences_weights__()
-        smallest_weights = heapq.nlargest(sentences_count, weights)
-        self.__print_result_text__(weights, smallest_weights)
+        largest_weights = heapq.nlargest(sentences_count, weights)
+        print(largest_weights)
+        self.__print_result_text__(weights, largest_weights)
 
     def __print_result_text__(self, weights, smallest_weights):
         print("_________________________________________________")
@@ -29,7 +32,8 @@ class Abstractor:
     def __get_sentence_weight__(self, sentence):
         words = get_words(sentence)
         if len(words) > 0:
-            return math.sqrt(sum([(math.pow(self.world_storage.get(word) / len(words), 2)) for word in words]))
+            storage = self.word_storage.storage
+            return sum([(math.pow(float(self.word_storage.get(word)) / len(storage), 2)) for word in sentence])
         return float('inf')
 
     def __get_sentences_weights__(self):
