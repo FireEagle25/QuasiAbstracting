e6dc0209264e99bae9124f53c9bfedf73f0c6dcc
WinPooh32 <winpooh32@yandex.ru>
2016-12-06 22:32:35 +1000

    Вывод предложений в том же порядке, что и в тексте

--- Abstractor/Abstractor.py
+++ Abstractor/Abstractor.py
@@ -20,8 +20,8 @@ class Abstractor:
         sentences_count = sentences_count if sentences_count > 0 else 1
 
         weights = self.__get_sentences_weights__()
-        largest_weights = heapq.nlargest(sentences_count, weights)
-        print(largest_weights)
+        largest_weights = list(set(weights)-set(heapq.nsmallest(sentences_count, weights)))
+
         self.__print_result_text__(weights, largest_weights)
 
     def __print_result_text__(self, weights, smallest_weights):
@@ -35,6 +35,14 @@ class Abstractor:
             storage = self.word_storage.storage
             return sum([(math.pow(float(self.word_storage.get(word)) / len(storage), 2)) for word in sentence])
         return float('inf')
+
+    def __get_sqr_distance__(self, sentence_x, sentence_y):
+        if len(sentence_x) < len(sentence_y):
+            sentence_x += [0 for i in range(len(sentence_y) - len(sentence_x))]
+        else:
+            sentence_y += [0 for i in range(len(sentence_x) - len(sentence_y))]
+
+        return sum(sentence_x - sentence_y) ** 2
 
     def __get_sentences_weights__(self):
         return [self.__get_sentence_weight__(sentence) for sentence in self.sentences]
